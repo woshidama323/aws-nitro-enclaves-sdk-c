0328bba0dbaf1f3815aa3883af4a7a67a597c52c
Harry Ma <machengxue707@gmail.com>
2022-09-03 21:36:11 +0800

    fix  container files

--- bin/kmstool-enclave-new-cli/build.sh
+++ bin/kmstool-enclave-new-cli/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build --target kmstool_enclave_new_cli -t kmstool_enclave_new_cli -f ../../containers/Dockerfile.al2 ../..
+docker build --target kmstool-enclave-new-cli -t kmstool-enclave-new-cli -f ../../containers/Dockerfile.al2 ../..
 CONTAINER_ID=$(docker create kmstool_enclave_new_cli)
 docker cp $CONTAINER_ID:/kmstool_enclave_new_cli ./
 docker cp $CONTAINER_ID:/usr/lib64/libnsm.so ./
