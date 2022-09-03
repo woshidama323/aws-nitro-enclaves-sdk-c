5a77e8d55dde1d2365daaa97a68a758e0088a7ad
Harry Ma <machengxue707@gmail.com>
2022-09-03 22:37:01 +0800

    fix docker-create  name

--- bin/kmstool-enclave-new-cli/build.sh
+++ bin/kmstool-enclave-new-cli/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 docker build --target kmstool-enclave-new-cli -t kmstool-enclave-new-cli -f ../../containers/Dockerfile.al2 ../..
-CONTAINER_ID=$(docker create kmstool_enclave_new_cli)
+CONTAINER_ID=$(docker create kmstool-enclave-new-cli)
 docker cp $CONTAINER_ID:/kmstool_enclave_new_cli ./
 docker cp $CONTAINER_ID:/usr/lib64/libnsm.so ./
 docker rm $CONTAINER_ID
